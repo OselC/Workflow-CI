a6f68c18821bf45b402c844224da3297d22dc57b
Osel Citta Chen <osel.citta.chen@gmail.com>
2026-04-24 17:19:02 +0700

    Added Code

---
+++ MLProject/modelling.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import mlflow
+import mlflow.xgboost
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+
+def train_model():
+    # Load Data
+    df = pd.read_csv("liver_patient_processing.csv")
+
+    # Split data
+    X = df.drop(columns=["Selector"])
+    y = df["Selector"]
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Autolog
+    mlflow.autolog()
+
+    # MLflow Tracking
+    mlflow.set_experiment("XGBoost_Liver_Disease_Tracking")
+
+    with mlflow.start_run():
+        model = XGBClassifier()
+        model.fit(x_train, y_train)
+
+        # Evaluation
+        model.score(x_test, y_test)
+
+        mlflow.last_active_run()
+
+        print("Training completed!")
+
+if __name__ == "__main__":
+    train_model()

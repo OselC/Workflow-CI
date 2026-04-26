989ce9280784dd29a9281d4a5362a72627933e37
Osel Citta Chen <osel.citta.chen@gmail.com>
2026-04-26 13:11:40 +0700

    Updated modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,9 +2,15 @@ import pandas as pd
 import mlflow
 import mlflow.xgboost
 from xgboost import XGBClassifier
+import dagshub
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import argparse
 
 def train_model():
+    dagshub.init(repo_owner='OselC', repo_name='Liver-Disease-Detection', mlflow=True)
+
     # Load Data
     df = pd.read_csv("liver_patient_preprocessing.csv")
 
@@ -26,9 +32,28 @@ def train_model():
         # Evaluation
         model.score(x_test, y_test)
 
+        # Confusion Matrix
+        y_pred = model.predict(x_test)
+        cm = confusion_matrix(y_test, y_pred)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
+        plt.savefig("confusion_matrix.png")
+        mlflow.log_artifact("confusion_matrix.png")
+
+        # Feature Importance
+        plt.figure(figsize=(10,6))
+        pd.Series(model.feature_importances_, index=X.columns).nlargest(10).plot(kind='barh')
+        plt.savefig("feature_importance.png")
+        mlflow.log_artifact("feature_importance.png")
+
         mlflow.last_active_run()
 
         print("Training completed!")
 
 if __name__ == "__main__":
-    train_model()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_estimators", type=int, default=100)
+    parser.add_argument("--learning_rate", type=float, default=0.1)
+    args = parser.parse_args()
+
+    train_model(args.n_estimators, args.learning_rate)
